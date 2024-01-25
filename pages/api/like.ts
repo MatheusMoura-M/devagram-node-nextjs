04838d7791b485d6f304ea8a034e06527bcdd1a9
Matheus M <matheus150101miranda@gmail.com>
2024-01-25 10:46:31 -0300

    feat: like/deslike api finished

---
+++ pages/api/like.ts
@@ -0,0 +1,65 @@
+import { conectarMongoDB } from "@/middlewares/conectarMongoDB";
+import { validarTokenJWT } from "@/middlewares/validarTokenJWT";
+import { PublicacaoModel } from "@/models/publicacaoModel";
+import { UsuarioModel } from "@/models/usuarioModel";
+import { TRespostaPadrao } from "@/types/respostaPadrao";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const endpointLike = async (
+  req: NextApiRequest,
+  res: NextApiResponse<TRespostaPadrao | any[]>
+) => {
+  try {
+    if (req.method === "PUT") {
+      const { id } = req.query;
+      const publicacao = await PublicacaoModel.findById(id);
+
+      if (!publicacao) {
+        return res.status(400).json({ erro: "Publicação não encontrada" });
+      }
+
+      const { userId } = req.query;
+      const usuario = await UsuarioModel.findById(userId);
+
+      if (!usuario) {
+        return res.status(400).json({ erro: "Usuário não encontrado" });
+      }
+
+      const indexDoUsuarioNoLike = publicacao.likes.findIndex(
+        (e: any) => e.toString() === usuario._id.toString()
+      );
+
+      if (indexDoUsuarioNoLike !== -1) {
+        publicacao.likes.splice(indexDoUsuarioNoLike, 1);
+
+        await PublicacaoModel.findByIdAndUpdate(
+          { _id: publicacao._id },
+          publicacao
+        );
+
+        return res
+          .status(200)
+          .json({ erro: "Publicação descurtida com sucesso" });
+      } else {
+        publicacao.likes.push(usuario._id);
+
+        await PublicacaoModel.findByIdAndUpdate(
+          { _id: publicacao._id },
+          publicacao
+        );
+
+        return res.status(200).json({ erro: "Publicação curtida com sucesso" });
+      }
+    }
+
+    return res.status(405).json({ erro: "Metódo informado não é válido" });
+  } catch (e) {
+    console.error(e);
+
+    return res
+      .status(500)
+      .json({ erro: "Ocorreu um erro ao curtir/descurtir uma publicação" + e });
+  }
+};
+
+export default validarTokenJWT(conectarMongoDB(endpointLike));
--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -10,22 +10,35 @@ const endpointPesquisa = async (
 ) => {
   try {
     if (req.method === "GET") {
-      const { filtro } = req.query;
+      if (req?.query?.id) {
+        const { id } = req.query;
+        const usuarioEncontrado = await UsuarioModel.findById(id);
 
-      if (!filtro || filtro.length < 2) {
-        return res
-          .status(405)
-          .json({ erro: "Informar pelo menos 2 caracteres" });
+        if (!usuarioEncontrado) {
+          return res.status(400).json({ erro: "Usuário não encontrado" });
+        }
+
+        usuarioEncontrado.senha = null;
+
+        return res.status(200).json(usuarioEncontrado);
+      } else {
+        const { filtro } = req.query;
+
+        if (!filtro || filtro.length < 2) {
+          return res
+            .status(405)
+            .json({ erro: "Informar pelo menos 2 caracteres" });
+        }
+
+        const usuariosEncontrados = await UsuarioModel.find({
+          $or: [
+            { nome: { $regex: filtro, $options: "i" } },
+            { email: { $regex: filtro, $options: "i" } },
+          ],
+        });
+
+        return res.status(200).json(usuariosEncontrados);
       }
-
-      const usuariosEncontrados = await UsuarioModel.find({
-        $or: [
-          { nome: { $regex: filtro, $options: "i" } },
-          { email: { $regex: filtro, $options: "i" } },
-        ],
-      });
-
-      return res.status(200).json(usuariosEncontrados);
     }
 
     return res.status(405).json({ erro: "Metódo informado não é válido" });
